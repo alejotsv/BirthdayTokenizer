ed39e2018191ac90f5cc62c0d35e3ec9c10a7b72
Alejandro Salas <asalas.wd@outlook.com>
2022-10-09 21:54:17 -0400

    Add tokenizer method

--- src/BirthdayTokenizer.java
+++ src/BirthdayTokenizer.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class BirthdayTokenizer {
     public static void main(String[] args) {
-        System.out.println("This is the tokenizer");
+        System.out.println("What is your birthday (MM/DD/YYYY)");
+        Scanner scanner = new Scanner(System.in);
+        String birthday = scanner.nextLine();
+
+        tokenizeBirthday(birthday);
+
+    }
+
+    static String[] tokenizeBirthday(String birthday) {
+        String[] finalArr = null;
+        StringTokenizer tokenizer = new StringTokenizer(birthday, "/");
+        System.out.println(tokenizer.countTokens());
+
+        return finalArr;
     }
 }
